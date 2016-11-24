32f3f040676fbd95de99cb752b7f47e03a2133c4
Naglis Jonaitis <naglis@mailbox.org>
2016-11-24 22:03:10 +0200

    Bump version: 0.1.0 → 0.1.1

--- pyisaf/__init__.py
+++ pyisaf/__init__.py
@@ -4,6 +4,6 @@ from .isaf_schema.v1_2 import schema_v1_2
 from .builder.v1_2 import ISAF1_2Builder
 
 __author__ = 'Naglis Jonaitis'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ['ISAF1_2Builder', 'schema_v1_2', '__author__', '__version__']
